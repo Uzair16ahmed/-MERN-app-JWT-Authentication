f082af5310ead27b85969caa8be16806d0fdefc7
Uzair16ahmed <khanuzair285@gmail.com>
2023-11-29 17:27:18 +0500

    1 hour 56 minutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,10 +10,14 @@ import {
     getUserProfile,
     updateUserProfile
 } from "../controllers/userController.js";
+import { protect } from "../middleware/authmiddleware.js";
 
 router.post('/', registerUser);
 router.post('/auth', authUser);
 router.post('/logout', logoutUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
+router
+.route('/profile')
+.get(protect, getUserProfile)
+.put(protect, updateUserProfile);
 
 export default router;
