7661e088061387770513d440699f2a5c62d12655
Uzair16ahmed <khanuzair285@gmail.com>
2024-06-04 00:32:19 +0500

    ...

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,6 +2,8 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import { apiSlice } from './slices/apiSlice';
 
+// using dispatch to trigger redux actions, which would then be processed by 
+//reducers to update the global state managed by redux.
 
 const store = configureStore({
     reducer: {
