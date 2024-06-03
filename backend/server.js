224f7364b4b065554f16c005ed6d77b2f0c07ed9
Uzair16ahmed <khanuzair285@gmail.com>
2024-06-04 00:32:28 +0500

    ...

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
-dotenv.config();
 import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
-const port = process.env.PORT || 5000;
 import userRoutes from './routes/userRoutes.js';
+dotenv.config();
+const port = process.env.PORT || 5000;
 
 connectDB();
 
@@ -28,7 +28,7 @@ if(process.env.NODE_ENV === 'production') {
 
     app.get('*',(req, res) => res.sendfile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
 }  else {
-    app.get('/', (req,res) => res.send('Server is ready'));
+    app.get('/', (req,res) => res.send('Server is ready')); 
 }
 //*********** **********/
 // app.get('/', (req,res) => res.send('Server is ready'));
